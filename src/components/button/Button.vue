b436865f03359a59284460022fe744a263866be8
Unknown <dnewhall@fishbowlsolutions.com>
2019-04-24 14:17:21 -0500

    start setting up npm module and build process

---
+++ src/components/button/Button.vue
@@ -0,0 +1,60 @@
+<template>
+    <component
+        :is="tag"
+        class="button"
+        :type="nativeType"
+        :class="[size, type, {
+            'rounded': rounded,
+            'loading': loading,
+            'outline': outline,
+            'focused': focused,
+            'active': active,
+            'hover': hover,
+            'selected': selected
+        }]"
+        @click="$emit('click', $event)"
+    >
+        <span v-if="label">{{label}}</span>
+        <span v-else-if="$slots.default"><slot /></span>
+    </component>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        type: [String, Object],
+        size: String,
+        label: String,
+        rounded: Boolean,
+        loading: Boolean,
+        outline: Boolean,
+        focused: Boolean,
+        active: Boolean,
+        hover: Boolean,
+        selected: Boolean,
+        nativeType: {
+            type: String,
+            default: 'button',
+            validator: (value) => {
+                return [
+                    'button',
+                    'submit',
+                    'reset'
+                ].indexOf(value) >= 0
+            }
+        },
+        tag: {
+            type: String,
+            default: 'button',
+            validator: (value) => {
+                return [
+                    'button',
+                    'a',
+                    'input'
+                ].indexOf(value) >= 0
+            }
+        }
+    }
+}
+</script>
